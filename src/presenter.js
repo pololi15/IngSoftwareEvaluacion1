55b2ff97eedd5ef9dd5c946d358800e40e22d36b
Ostin <ostinrivas15@gmail.com>
2025-09-11 01:30:27 -0400

    Ya esta funcional todo

--- src/presenter.js
+++ src/presenter.js
@@ -1,33 +1,30 @@
-import sumar from "./sumador";
-import { salida, ingreso, tarifa} from "./parqueo.js";
+import { salida, ingreso, tarifa } from "./parqueo.js";
 
 const first = document.querySelector("#ingreso-numero");
 const second = document.querySelector("#salida-numero");
+const days = document.querySelector("#dias-numero");
 const form = document.querySelector("#parqueo-form");
 const div = document.querySelector("#resultado-div");
 
-/*
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
-});
-*/
 form.addEventListener("submit", (event) => {
   event.preventDefault(); 
 
   const horaIngreso = Number.parseInt(first.value);
   const horaSalida = Number.parseInt(second.value);
+  const dias = Number.parseInt(days.value);
   const ticketPerdido = document.querySelector("#ticket-perdido").checked;
 
+  let monto = tarifa(ticketPerdido, horaIngreso, horaSalida, dias);
+
   if(ticketPerdido){
-    div.innerHTML = "<p>La tarifa por ticket perdido es: " + tarifa(ticketPerdido) + "</p>";
+    div.innerHTML = `<p>Ticket perdido → Total a pagar: <strong>${monto} Bs</strong></p>`;
     return;
   }
 
-  div.innerHTML = "<p>Hora de ingreso: " + ingreso(horaIngreso) + " " + "Hora de salida: " + salida(horaSalida)+ "</p>";
-
+  div.innerHTML = `
+    <p>Hora de ingreso: ${ingreso(horaIngreso)}:00</p>
+    <p>Hora de salida: ${salida(horaSalida)}:00</p>
+    <p>Días completos: ${dias}</p>
+    <p>Total a pagar: <strong>${monto} Bs</strong></p>
+  `;
 });
